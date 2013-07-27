2ff9bea3c9652d2e2d43551d4eae7e7f475637af
JohnnyPP <johnnnyppp@gmail.com>
2013-07-27 19:08:34 +0200

    6 Counters with up to 6 Measurements and 1 adjustment

--- CounterTrigger/ViewModel.cs
+++ CounterTrigger/ViewModel.cs
@@ -11,11 +11,24 @@ namespace CounterTrigger
     [ImplementPropertyChanged]
     class ViewModel
     {
+
+        #region Properties
         public DateTime TimeCommand { get; set; }
         public int Counter { get; set; }
         public int CounterDiv2 { get; set; }
         public int CounterDiv3 { get; set; }
-        public string CounterString { get; set; }
+        public int CounterDiv4 { get; set; }
+        public int CounterDiv5 { get; set; }
+        public int CounterDiv6 { get; set; }
+        public int CounterTemp { get; set; }
+        public string CounterString { get; set; } 
+        #endregion
+
+
+        #region Fields
+        bool Reset = false;
+        int TempCounter = 1; 
+        #endregion
 
         public ViewModel()
         {
@@ -23,6 +36,9 @@ namespace CounterTrigger
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer.Start();
+            Counter = 0;
+            CounterTemp = 0;
+
         }
 
         /// <summary>
@@ -32,20 +48,136 @@ namespace CounterTrigger
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
-            TimeCommand = DateTime.Now;
-            Counter++;
-            CounterDiv2 = Counter / 2;
-            CounterDiv3 = Counter / 3;
+            Counters();
 
-            if ((Counter % 2) == 0)
+            Measurement3();
+            
+            MeasurementDisplay();
+
+        }
+
+        private void Counters()
+        {
+            TimeCommand = DateTime.Now;
+
+            if (Reset == false)
             {
-                CounterString = "Counter 1 is even";
+                CounterDiv2 = Counter / 2;
+                CounterDiv3 = Counter / 3;
+                CounterDiv4 = Counter / 4;
+                CounterDiv5 = Counter / 5;
+                CounterDiv6 = Counter / 6;
+                Counter++;
             }
             else
             {
-                CounterString = "Counter 1 is odd";
+                Counter = 0;
+                CounterDiv2 = Counter / 2;
+                CounterDiv3 = Counter / 3;
+                CounterDiv4 = Counter / 4;
+                CounterDiv5 = Counter / 5;
+                CounterDiv6 = Counter / 6;
+                Counter++;
+                Reset = false;
             }
+        }
 
-        } 
+        private void MeasurementDisplay()
+        {
+            if ((CounterTemp % 2) == 0)
+            {
+                CounterString = "Measurement " + TempCounter.ToString();
+                TempCounter++;
+            }
+            else
+            {
+                CounterString = "Adjustment";
+                TempCounter = 1;
+            }
+        }
+
+        private void Measurement1()
+        {
+            if (Counter % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = Counter;
+            }
+        }
+
+        private void Measurement2()
+        {
+            if (CounterDiv2 % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = CounterDiv2;
+            }
+        }
+
+        private void Measurement3()
+        {
+            if (CounterDiv3 % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = CounterDiv3;
+            }
+        }
+
+        private void Measurement4()
+        {
+            if (CounterDiv4 % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = CounterDiv4;
+            }
+        }
+
+        private void Measurement5()
+        {
+            if (CounterDiv5 % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = CounterDiv5;
+            }
+        }
+
+        private void Measurement6()
+        {
+            if (CounterDiv6 % 2 != 0)
+            {
+                CounterTemp++;
+                Counter++;
+                Reset = true;
+            }
+            else
+            {
+                CounterTemp = CounterDiv6;
+            }
+        }
     }
 }
