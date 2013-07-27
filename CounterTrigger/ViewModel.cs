ce135f3648bd199fd4113db11571ab01faf5812c
JohnnyPP <johnnnyppp@gmail.com>
2013-07-27 03:52:17 +0200

    Initial release with Fody PropertyChanged. The warnings after installation of Fody can be removed by closing all the windows in visual studio and rebuilding the solution:
    http://stackoverflow.com/questions/7061556/has-invalid-child-element-warnings-in-microsoft-common-targets-while-building

---
+++ CounterTrigger/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace CounterTrigger
+{
+    [ImplementPropertyChanged]
+    class ViewModel
+    {
+        public DateTime TimeCommand { get; set; }
+        public int Counter { get; set; }
+
+        public ViewModel()
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Start();
+        }
+
+        /// <summary>
+        /// Handles the Tick event of the dispatcherTimer control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            TimeCommand = DateTime.Now;
+            Counter++;
+        } 
+    }
+}
